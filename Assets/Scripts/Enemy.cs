94caf4873777a4a70f7d5efe51bb2602670840bf
alina <mlkysheikh@gmail.com>
2023-02-01 22:21:28 -0500

    Added summary tags to Enemy class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Enemy : Unit
 {
+    /// <summary>
+    /// The unit's elemental weaknesses, which includes ice, fire, lightning, wind, or earth.
+    /// </summary>
     public List<ElementType> weaknesses;
+
+    /// <summary>
+    /// The unit's elemental immunities, which includes ice, fire, lightning, wind, or earth.
+    /// </summary>
     public List<ElementType> immunities;
 }
