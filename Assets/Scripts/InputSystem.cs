be81090a3483923b459986f72a4ab39665b5bf32
alina <mlkysheikh@gmail.com>
2023-03-15 22:44:32 +0000

    downgrade dotnet version

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -6,18 +6,25 @@ using UnityEngine.InputSystem;
 public class InputSystem : MonoBehaviour
 {
     private Rigidbody2D player;
+    private PlayerInput playerInput;
+
     private bool isGrounded = true;
 
     private void Awake()
     {
         player = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.PlayerMovement.Enable();
+        playerInputActions.PlayerMovement.Jump.performed += Jump;
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed && isGrounded)
         {
-            Debug.Log("Jumping! " + isGrounded);
+            Debug.Log("Jumping!");
             player.AddForce(Vector3.up * 5f, ForceMode2D.Impulse);
         }
     }
