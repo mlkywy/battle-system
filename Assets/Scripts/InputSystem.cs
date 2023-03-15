e4e6b367003e37771e0546edf5e7015149d9c72c
alina <mlkysheikh@gmail.com>
2023-03-15 23:00:10 +0000

    movement

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -7,17 +7,24 @@ public class InputSystem : MonoBehaviour
 {
     private Rigidbody2D player;
     private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
 
     private bool isGrounded = true;
+    private float speed = 5f;
 
     private void Awake()
     {
         player = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInputActions();
 
-        PlayerInputActions playerInputActions = new PlayerInputActions();
         playerInputActions.PlayerMovement.Enable();
         playerInputActions.PlayerMovement.Jump.performed += Jump;
+    }
+
+    private void FixedUpdate() 
+    {
+        Movement();
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -25,7 +32,7 @@ public class InputSystem : MonoBehaviour
         if (context.performed && isGrounded)
         {
             Debug.Log("Jumping!");
-            player.AddForce(Vector3.up * 5f, ForceMode2D.Impulse);
+            player.AddForce(Vector3.up * speed, ForceMode2D.Impulse);
         }
     }
 
@@ -37,5 +44,11 @@ public class InputSystem : MonoBehaviour
     private void OnCollisionExit2D(Collision2D ground) 
     {
         isGrounded = false;
+    }
+
+    public void Movement()
+    {
+        Vector2 inputVector = playerInputActions.PlayerMovement.Movement.ReadValue<Vector2>();
+        player.AddForce(new Vector3(inputVector.x, 0, inputVector.y), ForceMode2D.Impulse);
     }
 }
